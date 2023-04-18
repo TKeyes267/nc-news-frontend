3d9bb1547158380a6d210a810482d5c18f2b15d4
TKeyes267 <tomkeyes26@gmail.com>
2023-04-18 10:03:31 +0100

    add isLoading

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,28 +2,35 @@ import { useState, useEffect } from "react";
 import { getArticles } from "../Api";
 
 const ArticleList = ({ articles, setArticles }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     getArticles().then((res) => {
-      console.log(res);
       setArticles(res.articles);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <main className="ArticleList">
-      {/* <h2>ARTICLES HERE</h2> */}
+      <h2>ARTICLES</h2>
       <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article.article_id}>
-              <div>
-                <p>{article.author}</p>
-                <p>{article.title}</p>
-                <img src={article.article_img_url} />
-              </div>
-            </li>
-          );
-        })}
+        {isLoading ? (
+          <li>Loading</li>
+        ) : (
+          articles.map((article) => {
+            return (
+              <li key={article.article_id}>
+                <div>
+                  <p>{article.author}</p>
+                  <p>{article.title}</p>
+                  <img src={article.article_img_url} />
+                </div>
+              </li>
+            );
+          })
+        )}
       </ul>
     </main>
   );
